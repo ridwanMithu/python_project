acb7b793a8b9e8f552065a03aa5d702baa55df28
ridwanmithu <ridwan.mithu@outlook.com>
2025-01-13 00:45:12 +0600

    update to number guessing

--- PythonProjectsP2.py
+++ PythonProjectsP2.py
@@ -2,22 +2,16 @@ import random
 
 actual=random.randint(1,100)
 print(actual)
-playing=True
-while playing:
+while True:
   guess=input("Guess the number: ")
   try:
     guess=int(guess)
+    if guess==actual:
+      print("You have guessed Correctly")
+      break
+    elif guess<actual:
+      print("Think More")
+    elif guess>actual:
+      print('Think less')
   except ValueError:
-    guess=input("Guess the number: ")
-  
-  guess=int(guess)
-  if guess==actual:
-    print("You have guessed Correctly")
-    playing=False
-    break
-  elif guess<=actual:
-    print("Think More")
-  elif guess>=actual:
-    print('Think less')
-  elif ValueError:
-    print("Try whole number")
+    print("Guess a whole number")
